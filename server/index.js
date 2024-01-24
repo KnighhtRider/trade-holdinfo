643232b0eb02e09cb546eaed67b5d17f16751ca7
Vivek Thakur <110936458+KnighhtRider@users.noreply.github.com>
2024-01-24 15:28:57 +0530

    :fire: Project Completed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const mongoose = require('mongoose');
+var cors = require('cors')
 
 const app = express(); 
 const PORT = 5000;
+
+app.use(cors())
 
 const mongoURL = 'mongodb+srv://vivekthakur6259:vivek123456@trade-center.lhlmgow.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoURL);
@@ -22,32 +25,32 @@ const tradeSchema = new mongoose.Schema({
 const Trade = mongoose.model('Trade', tradeSchema);
 
 
-// app.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
   
-//   const api_data = await axios.get('https://api.wazirx.com/api/v2/tickers');
+  const api_data = await axios.get('https://api.wazirx.com/api/v2/tickers');
 
-//     // Extract the top 10 trade data from the api_data
-//     const data = Object.values(api_data.data).slice(0, 10); 
+    // Extract the top 10 trade data from the api_data
+    const data = Object.values(api_data.data).slice(0, 10); 
 
-//     // Save each top 10 trade data to the database
-//     for (const tradeData of data) {
-//       const trade = new Trade({
-//         name: tradeData.name,
-//         last: tradeData.last,
-//         buy: tradeData.buy,
-//         sell: tradeData.sell,
-//         volume: tradeData.volume,
-//         base_unit: tradeData.baseUnit,
-//       });
+    // Save each top 10 trade data to the database
+    for (const tradeData of data) {
+      const trade = new Trade({
+        name: tradeData.name,
+        last: tradeData.last,
+        buy: tradeData.buy,
+        sell: tradeData.sell,
+        volume: tradeData.volume,
+        base_unit: tradeData.base_unit,
+      });
 
-//       await trade.save();
-//     }
+      await trade.save();
+    }
 
     
-//     // Send the top 10 trade data as a JSON response
-//     res.json(data);
+    // Send the top 10 trade data as a JSON response
+    res.json(data);
 
-// })
+})
 
 // Create a new route to fetch the stored data from the database
 
@@ -56,10 +59,10 @@ const Trade = mongoose.model('Trade', tradeSchema);
 app.get('/trades', async (req, res) => {
   try {
     // Fetch all trade data from the database
-    const tradesFromDB = await Trade.find();
+    const data = await Trade.find();
 
     // Send the fetched trade data as a JSON response to the frontend
-    res.json(tradesFromDB);
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import tradeLogo from "./assets/trade-logo.png";
+import telegramLogo from "./assets/telegram-logo.png";
 import axios from "axios";
+import logo_1 from "./assets/logo-1.png";
+import logo_2 from "./assets/log-2.png";
+import logo_3 from "./assets/logo-3.png";
 
+function App() {
+  const [trades, setTrades] = useState([]);
 
+  const [countdown, setCountdown] = useState(60);
 
-function App() {  
-  const [trades, setTrades] = useState([]); 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCount) => (prevCount > 0 ? prevCount - 1 : 60));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    // Fetch data from the /trades endpoint
+    axios.get("http://localhost:5000/trades").then((response) => {
+      setTrades(response.data);
+    });
+  }, []);
 
   return (
     <div className="theme-dark">
       {/* Header  */}
-      <div className="App">
+      <div className="">
         <div style={{ padding: "20px 30px 0px" }}>
           <div className="align-items-center justify-content-sm-center row">
             <div className="text-center-sm col-12 col-sm-7 col-md-5 col-lg-4">
@@ -26,8 +46,8 @@ function App() {
               </a>
             </div>
 
-            <div className="text-center padding-none col-12 col-sm-12 col-md-2 col-lg-4">
-              <div className="btn-group">
+            <div className="text-center padding-none col-12 col-sm-12 col-md-2 col-lg-4 ">
+              <div className="btn-group px-2">
                 <button
                   type="button"
                   aria-haspopup="true"
@@ -54,7 +74,7 @@ function App() {
                 </div>
               </div>
 
-              <div className="btn-group">
+              <div className="btn-group px-2">
                 <button
                   type="button"
                   aria-haspopup="true"
@@ -82,7 +102,7 @@ function App() {
                 </div>
               </div>
 
-              <div className="btn-group">
+              <div className="btn-group px-2">
                 <a
                   target="_blank"
                   href="https://wazirx.com/invite/sp7pvbt6?utm_source=finstreet&utm_medium=affiliate&utm_campaign=regnow-btn"
@@ -100,12 +120,41 @@ function App() {
               <div className="d-inline-flex flex-wrap align-items-center justify-content-center">
                 <div className="progress-bar-wrapper">
                   {/* Your circular progress bar SVG */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 100 100"
+                    width="100"
+                    height="50"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <circle cx="50" cy="50" r="48" stroke="#f8f9fa" />
+                    <text
+                      x="50%"
+                      y="50%"
+                      dominantBaseline="middle"
+                      textAnchor="middle"
+                      fontSize="40"
+                      fill="#f8f9fa"
+                    >
+                      {countdown}
+                    </text>
+                  </svg>
                 </div>
-                <a className="color-white" href="/connect/telegram">
-                  <div className="d-flex telegram-logo-text header-telegram-button btn align-items-center pointer color-white">
+                <a
+                  className="color-white text-decoration-none"
+                  href="/connect/telegram"
+                >
+                  <div
+                    className="d-flex telegram-logo-text header-telegram-button btn align-items-center pointer color-white "
+                    style={{ backgroundColor: "skyblue" }}
+                  >
                     <div className="d-inline-block">
                       <img
-                        src="data:image/png"
+                        src={telegramLogo}
                         className="telegram-logo"
                         alt="Telegram Logo"
                       />
@@ -136,16 +185,21 @@ function App() {
           >
             <div className="text-center">
               <div className="average-header-maintext color-green">0.9 %</div>
-              <div className="average-header-subHeading">5 Mins</div>
+              <div className="average-header-subHeading" style={{ color: "grey" }}>5 Mins</div>
             </div>
             <div className="text-center">
               <div className="average-header-maintext color-green">1.78 %</div>
-              <div className="average-header-subHeading">1 Hour</div>
+              <div className="average-header-subHeading" style={{ color: "grey" }}>1 Hour</div>
             </div>
             <div style={{ maxWidth: "40%" }}>
               <div className="text-center font-32 average-block">
                 <div className="average-subText">
-                  <span className="subText-heading">Best Price to Trade</span>
+                  <span
+                    className="subText-heading"
+                    style={{ fontSize: "25px", color: "grey" }}
+                  >
+                    Best Price to Trade
+                  </span>
                 </div>
                 <div
                   className="average-heading"
@@ -153,28 +207,27 @@ function App() {
                 >
                   ₹ 35,97,911
                 </div>
-                <div className="average-subText">
+                <div className="average-subText" style={{ color: "grey" }}>
                   Average BTC/INR net price including commission
                 </div>
               </div>
             </div>
             <div className="text-center">
               <div className="average-header-maintext color-green">8.83 %</div>
-              <div className="average-header-subHeading">1 Day</div>
+              <div className="average-header-subHeading" style={{ color: "grey" }}>1 Day</div>
             </div>
             <div className="text-center">
               <div className="average-header-maintext color-green">10.45 %</div>
-              <div className="average-header-subHeading">7 Days</div>
+              <div className="average-header-subHeading" style={{ color: "grey" }}>7 Days</div>
             </div>
           </div>
-
 
           {/* Responsive Data Fetching */}
           <div
             className="fiat-crypto-table table-responsive"
             style={{ margin: "0px auto" }}
           >
-            <table className="table table-borderless text-center">
+            <table className="table-borderless text-center">
               <thead>
                 <tr>
                   <th>
@@ -209,8 +262,44 @@ function App() {
                   </th>
                 </tr>
               </thead>
-              <tbody>{/* Your table rows */}</tbody>
+              <tbody>
+                {trades.map((trade, index) => (
+                  <tr key={index}>
+                    <td className="">{index + 1}</td>
+                    <td>
+                      {/* <a href={} target="_blank"> */}
+                      <img
+                        className="exchange-logo"
+                        src={logo_1}
+                        alt="Exchange Logo"
+                      />
+                      <span className="exchange-name">{trade.name}</span>
+                      {/* </a> */}
+                    </td>
+                    <td>₹ {trade.last}</td>
+                    <td>
+                      <span>
+                        ₹ {trade.buy} / ₹ {trade.sell}
+                      </span>
+                    </td>
+                    <td>{trade.volume}</td>
+                    <td>{trade.base_unit}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer">
+      <div class="d-flex align-items-center text-center">
+          <div class="footer-text">Copyright © 2019</div>
+          <div class="footer-text">HodlInfo.com</div>
+          <div class="footer-text pointer">
+            <a href="mailto:support@hodlinfo.com" class="footer-text-link">
+              Support
+            </a>
           </div>
         </div>
       </div>
